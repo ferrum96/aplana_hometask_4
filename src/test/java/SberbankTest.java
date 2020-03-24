470fd1f4811f773f2dbba8551dfcab832da27670
Константин <ferrum9696@mail.ru>
2020-03-24 17:35:55 +0300

    Task4

--- src/test/java/SberbankTest.java
+++ src/test/java/SberbankTest.java
@@ -4,6 +4,10 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
@@ -12,6 +16,7 @@ public class SberbankTest {
 
     private WebDriver driver;
     private String baseUrl;
+
 
     @Before
     public void setUp() throws Exception {
@@ -29,6 +34,8 @@ public class SberbankTest {
     @Test
     public void Test() throws Exception {
 
+        Wait<WebDriver> wait = new WebDriverWait(driver, 5, 1000);
+
         String regionTitleXpath = "//div[@class='hd-ft-region__title']/child::span";
         String inputRegionXpath = "//a[@class='kit-link kit-link_m hd-ft-region__city'][contains(text(),'Нижегородская область')]";
 
@@ -39,9 +46,8 @@ public class SberbankTest {
 
         assertEquals("Нижегородская область", driver.findElement(By.xpath("//div[@class='hd-ft-region__title']/child::span")).getText());
 
-        Thread.sleep(3000);
-
         WebElement lastElement = driver.findElement(By.xpath("//div[@data-pid='SiteFooter-5058887']"));
+        wait.until(ExpectedConditions.elementToBeClickable(lastElement));
         int y = lastElement.getLocation().getY();
         JavascriptExecutor js = (JavascriptExecutor)driver;
         js.executeScript("window.scrollTo(0,"+y+")");
